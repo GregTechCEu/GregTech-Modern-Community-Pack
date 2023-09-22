1dbd5aa8c0a96d12b13381ce8cdbe8955c773014
mina <143211829+ursamina@users.noreply.github.com>
2023-09-22 18:08:04 +0100

    fixed ae2 progression
    custom inscriber machine is no longer necessary, will be implemented in future unless folks really hate the idea

--- kubejs/server_scripts/gregtech/inscriber.js
+++ kubejs/server_scripts/gregtech/inscriber.js
@@ -1,28 +1,28 @@
 ServerEvents.recipes(event => {
   const greg = event.recipes.gtceu
   // Printed Silicon
-  greg.inscriber("printed_silicon_circuit")
+  greg.forming_press("printed_silicon_circuit")
     .itemInputs("#forge:plates/silicon")
     .notConsumable("ae2:silicon_press")
     .itemOutputs("ae2:printed_silicon")
     .duration(200).EUt(30)
   
   // Printed Logic Circuit
-  greg.inscriber("printed_logic_processor")
+  greg.forming_press("printed_logic_processor")
     .itemInputs("#forge:plates/gold")
     .notConsumable("ae2:logic_processor_press")
     .itemOutputs("ae2:printed_logic_processor")
     .duration(200).EUt(30)
 
   // Printed Engineering Circuit
-  greg.inscriber("printed_engineering_processor")
+  greg.forming_press("printed_engineering_processor")
     .itemInputs("#forge:plates/diamond")
     .notConsumable("ae2:engineering_processor_press")
     .itemOutputs("ae2:printed_engineering_processor")
     .duration(200).EUt(30)
 
   // Printed Calculation Circuit
-  greg.inscriber("printed_calculation_circuit")
+  greg.forming_press("printed_calculation_circuit")
     .itemInputs("#forge:plates/certus_quartz")
     .notConsumable("ae2:calculation_processor_press")
     .itemOutputs("ae2:printed_calculation_processor")
