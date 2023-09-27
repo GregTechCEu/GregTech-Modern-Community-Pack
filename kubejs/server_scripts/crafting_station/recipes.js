4d38b674b719f78d0ef1151bff8549952c37b533
mina <143211829+ursamina@users.noreply.github.com>
2023-09-27 04:16:06 +0100

    added sawing recipe for crafting station slab
    wasn't going to integrate the slab crafting table but gtnh reminded me it exists

--- kubejs/server_scripts/crafting_station/recipes.js
+++ kubejs/server_scripts/crafting_station/recipes.js
@@ -4,4 +4,9 @@ ServerEvents.recipes(event => {
     "minecraft:crafting_table",
     "#forge:tools/saws"
   ])
+  event.remove({id: "craftingstation:crafting_station_slab"})
+  event.shapeless("craftingstation:crafting_station_slab", [
+    "craftingstation:crafting_station",
+    "#forge:tools/saws"
+  ])
 })
