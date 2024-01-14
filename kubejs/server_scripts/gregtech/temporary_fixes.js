11bcccf8f415ea0dd3a1a2d8bdd561381f8c94d7
mina <143211829+ursamina@users.noreply.github.com>
2024-01-14 12:35:56 +0000

    fix missing obsidian dust recipe

---
+++ kubejs/server_scripts/gregtech/temporary_fixes.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes((event) => {
+  event.recipes.gtceu
+    .macerator("obsidian_dust")
+    .itemInputs("obsidian")
+    .itemOutputs("#forge:dusts/obsidian")
+    .EUt(2).duration(25)
+});
