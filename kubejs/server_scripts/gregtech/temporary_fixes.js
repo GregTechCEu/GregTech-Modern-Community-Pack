7cd004f718755064deae23d3b4f8bffc494b36a6
mina <143211829+ursamina@users.noreply.github.com>
2024-01-14 13:04:01 +0000

    temporary fix for brick wooden form being consumed

--- kubejs/server_scripts/gregtech/temporary_fixes.js
+++ kubejs/server_scripts/gregtech/temporary_fixes.js
@@ -3,5 +3,15 @@ ServerEvents.recipes((event) => {
     .macerator("obsidian_dust")
     .itemInputs("obsidian")
     .itemOutputs("#forge:dusts/obsidian")
-    .EUt(2).duration(25)
+    .EUt(2)
+    .duration(25);
+
+  event.remove("gtceu:shaped/compressed_coke_clay");
+  event
+    .shaped("3x gtceu:compressed_coke_clay", ["CCC", "ScS", "SSS"], {
+      C: "clay_ball",
+      c: "gtceu:brick_wooden_form",
+      S: "#minecraft:sand",
+    })
+    .keepIngredient("gtceu:brick_wooden_form");
 });
