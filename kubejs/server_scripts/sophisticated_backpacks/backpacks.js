aee198962c90e6a51f33b408684b9746a6deb2c8
mina <143211829+ursamina@users.noreply.github.com>
2024-02-22 16:47:24 +0000

    copper backpacks
    idk why sophis needed to add another tier but Ok
    
    wont break tiering

--- kubejs/server_scripts/sophisticated_backpacks/backpacks.js
+++ kubejs/server_scripts/sophisticated_backpacks/backpacks.js
@@ -1,5 +1,7 @@
 ServerEvents.recipes((event) => {
   event.remove({ id: "sophisticatedbackpacks:backpack" });
+  event.remove({ id: "sophisticatedbackpacks:copper_backpack" });
+  event.remove({ id: "sophisticatedbackpacks:iron_backpack_from_copper" });
   event.remove({ id: "sophisticatedbackpacks:iron_backpack" });
   event.remove({ id: "sophisticatedbackpacks:gold_backpack" });
   event.remove({ id: "sophisticatedbackpacks:diamond_backpack" });
@@ -29,10 +31,28 @@ ServerEvents.recipes((event) => {
       F: { item: "minecraft:string" },
       P: { tag: "forge:plates/invar" },
     },
+    result: { item: "sophisticatedbackpacks:copper_backpack" },
+  });
+
+  event.custom({
+    type: "sophisticatedbackpacks:backpack_upgrade",
+    conditions: [
+      {
+        itemRegistryName: "iron_backpack",
+        type: "sophisticatedbackpacks:item_enabled",
+      },
+    ],
+    pattern: ["SLS", "LBL", "FPF"],
+    key: {
+      S: { tag: "forge:screws/steel" },
+      L: { item: "minecraft:leather" },
+      B: { item: "sophisticatedbackpacks:copper_backpack" },
+      F: { item: "minecraft:string" },
+      P: { tag: "forge:plates/rose_gold" },
+    },
     result: { item: "sophisticatedbackpacks:iron_backpack" },
   });
 
-  event.remove({ id: "sophisticatedbackpacks:gold_backpack" });
   event.custom({
     type: "sophisticatedbackpacks:backpack_upgrade",
     conditions: [
@@ -43,16 +63,15 @@ ServerEvents.recipes((event) => {
     ],
     pattern: ["SLS", "LBL", "FPF"],
     key: {
-      S: { tag: "forge:screws/steel" },
+      S: { tag: "forge:screws/aluminium" },
       L: { item: "minecraft:leather" },
       B: { item: "sophisticatedbackpacks:iron_backpack" },
-      F: { item: "minecraft:string" },
-      P: { tag: "forge:plates/rose_gold" },
+      F: { item: "gtceu:carbon_fibers" },
+      P: { tag: "forge:plates/blue_alloy" },
     },
     result: { item: "sophisticatedbackpacks:gold_backpack" },
   });
 
-  event.remove({ id: "sophisticatedbackpacks:diamond_backpack" });
   event.custom({
     type: "sophisticatedbackpacks:backpack_upgrade",
     conditions: [
@@ -63,16 +82,15 @@ ServerEvents.recipes((event) => {
     ],
     pattern: ["SLS", "LBL", "FPF"],
     key: {
-      S: { tag: "forge:screws/aluminium" },
+      S: { tag: "forge:screws/stainless_steel" },
       L: { item: "minecraft:leather" },
       B: { item: "sophisticatedbackpacks:gold_backpack" },
       F: { item: "gtceu:carbon_fibers" },
-      P: { tag: "forge:plates/blue_alloy" },
+      P: { tag: "forge:plates/polytetrafluoroethylene" },
     },
     result: { item: "sophisticatedbackpacks:diamond_backpack" },
   });
 
-  event.remove({ id: "sophisticatedbackpacks:netherite_backpack" });
   event.custom({
     type: "sophisticatedbackpacks:backpack_upgrade",
     conditions: [
@@ -83,11 +101,11 @@ ServerEvents.recipes((event) => {
     ],
     pattern: ["SLS", "LBL", "FPF"],
     key: {
-      S: { tag: "forge:screws/stainless_steel" },
+      S: { tag: "forge:screws/titanium" },
       L: { item: "minecraft:leather" },
       B: { item: "sophisticatedbackpacks:diamond_backpack" },
       F: { item: "gtceu:carbon_fibers" },
-      P: { tag: "forge:plates/polytetrafluoroethylene" },
+      P: { tag: "forge:plates/epoxy" },
     },
     result: { item: "sophisticatedbackpacks:netherite_backpack" },
   });
