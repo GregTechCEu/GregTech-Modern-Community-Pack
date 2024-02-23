a0e877dc59a31fa797a5bb5cf97057cf60ad42ee
ursamina <143211829+ursamina@users.noreply.github.com>
2024-02-23 21:59:59 +0000

    big building block bonanza - all the building blocks

---
+++ kubejs/server_scripts/architects_palette.js
@@ -0,0 +1,446 @@
+ServerEvents.recipes((event) => {
+  event.remove({ output: "architects_palette:charcoal_block" });
+
+  event.remove({ output: "architects_palette:abyssaline" });
+  event.recipes.gtceu
+    .mixer("abyssaline")
+    .itemInputs("gtceu:obsidian_dust", "minecraft:prismarine_shard")
+    .itemOutputs("2x architects_palette:abyssaline")
+    .EUt(2)
+    .duration(20);
+
+  event.recipes.gtceu
+    .construction_core("abyssaline")
+    .itemInputs("gtceu:obsidian_dust", "minecraft:prismarine_shard")
+    .itemOutputs("6x architects_palette:abyssaline")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:plating_block" });
+  event.recipes.gtceu
+    .assembler("plating_block")
+    .itemInputs("4x #forge:plates/iron")
+    .itemOutputs("4x architects_palette:plating_block")
+    .circuit(10)
+    .EUt(2)
+    .duration(20);
+
+  event.recipes.gtceu
+    .construction_core("plating_block")
+    .itemInputs("4x #forge:plates/iron")
+    .itemOutputs("8x architects_palette:plating_block")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ output: "architects_palette:myonite" });
+  event.recipes.gtceu
+    .mixer("myonite")
+    .itemInputs("minecraft:stone", "#forge:mushrooms")
+    .itemOutputs("4x architects_palette:myonite")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("myonite")
+    .itemInputs("minecraft:stone", "#forge:mushrooms")
+    .itemOutputs("8x architects_palette:myonite")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(2)
+    .duration(20);
+
+  event.remove({ output: "architects_palette:olivestone_bricks" });
+  event.recipes.gtceu
+    .mixer("olivestone_bricks")
+    .itemInputs("minecraft:stone", "#forge:dyes/green")
+    .itemOutputs("4x architects_palette:olivestone_bricks")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("olivestone_bricks")
+    .itemInputs("minecraft:stone", "#forge:dyes/green")
+    .itemOutputs("8x architects_palette:olivestone_bricks")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(2)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:illuminated_olivestone" });
+  event.recipes.gtceu
+    .mixer("illuminated_olivestone")
+    .itemInputs("#architects_palette:olivestone", "minecraft:glowstone")
+    .itemOutputs("architects_palette:illuminated_olivestone")
+    .EUt(2)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:overgrown_algal_bricks" });
+  event.recipes.gtceu
+    .mixer("overgrown_algal_bricks")
+    .itemInputs("architects_palette:algal_bricks", "minecraft:kelp")
+    .itemOutputs("architects_palette:overgrown_algal_bricks")
+    .EUt(2)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:algal_lamp" });
+  event.recipes.gtceu
+    .mixer("algal_lamp")
+    .itemInputs("architects_palette:algal_brick", "minecraft:glowstone")
+    .itemOutputs("architects_palette:algal_lamp")
+    .EUt(2)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:lit_osseous_skull" });
+  event.recipes.gtceu
+    .canner("lit_osseous_skull")
+    .itemInputs("architects_palette:osseous_skull", "minecraft:torch")
+    .itemOutputs("architects_palette:lit_osseous_skull")
+    .EUt(2)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:lit_withered_osseous_skull" });
+  event.recipes.gtceu
+    .canner("lit_withered_osseous_skull")
+    .itemInputs(
+      "architects_palette:withered_osseous_skull",
+      "minecraft:soul_torch"
+    )
+    .itemOutputs("architects_palette:lit_withered_osseous_skull")
+    .EUt(2)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:gilded_sandstone" });
+  event.recipes.gtceu
+    .mixer("gilded_sandstone")
+    .itemInputs("minecraft:sandstone", "minecraft:gold_nugget")
+    .itemOutputs("2x architects_palette:gilded_sandstone")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("gilded_sandstone")
+    .itemInputs("minecraft:sandstone", "minecraft:gold_nugget")
+    .itemOutputs("4x architects_palette:gilded_sandstone")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:weeping_blackstone" });
+  event.recipes.gtceu
+    .mixer("weeping_blackstone")
+    .itemInputs("minecraft:blackstone", "minecraft:weeping_vines")
+    .itemOutputs("1x architects_palette:weeping_blackstone")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("weeping_blackstone")
+    .itemInputs("minecraft:blackstone", "minecraft:weeping_vines")
+    .itemOutputs("2x architects_palette:weeping_blackstone")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:choral_end_stone_bricks" });
+  event.recipes.gtceu
+    .mixer("twisting_blackstone")
+    .itemInputs("minecraft:blackstone", "minecraft:twisting_vines")
+    .itemOutputs("1x architects_palette:twisting_blackstone")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("twisting_blackstone")
+    .itemInputs("minecraft:blackstone", "minecraft:twisting_vines")
+    .itemOutputs("2x architects_palette:twisting_blackstone")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:choral_end_stone_bricks" });
+  event.recipes.gtceu
+    .mixer("choral_end_stone_bricks")
+    .itemInputs("minecraft:end_stone_bricks", "minecraft:chorus_fruit")
+    .itemOutputs("1x architects_palette:choral_end_stone_bricks")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("choral_end_stone_bricks")
+    .itemInputs("minecraft:end_stone_bricks", "minecraft:chorus_fruit")
+    .itemOutputs("2x architects_palette:choral_end_stone_bricks")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:sunstone" });
+  event.recipes.gtceu
+    .mixer("sunstone")
+    .itemInputs("minecraft:basalt", "architects_palette:sunmetal_blend")
+    .itemOutputs("2x architects_palette:sunstone")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("sunstone")
+    .itemInputs("minecraft:basalt", "architects_palette:sunmetal_blend")
+    .itemOutputs("4x architects_palette:sunstone")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:molten_nether_bricks" });
+  event.recipes.gtceu
+    .mixer("molten_nether_bricks")
+    .itemInputs("minecraft:nether_bricks", "minecraft:magma_block")
+    .itemOutputs("2x architects_palette:molten_nether_bricks")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("molten_nether_bricks")
+    .itemInputs("minecraft:nether_bricks", "minecraft:magma_block")
+    .itemOutputs("4x architects_palette:molten_nether_bricks")
+    .inputFluids("minecraft:lava 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:coarse_snow" });
+  event.recipes.gtceu
+    .mixer("coarse_snow")
+    .itemInputs("minecraft:snow_block", "minecraft:gravel")
+    .itemOutputs("2x architects_palette:coarse_snow")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("coarse_snow")
+    .itemInputs("minecraft:snow_block", "minecraft:gravel")
+    .itemOutputs("4x architects_palette:coarse_snow")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:unobtanium" });
+  event.recipes.gtceu
+    .macerator("unobtanium")
+    .itemInputs("architects_palette:unobtanium_block")
+    .itemOutputs("4x architects_palette:unobtanium")
+    .chancedOutput(Item.of("architects_palette:unobtanium", 1), 100, 0)
+    .EUt(GTValues.VA[GTValues.HV])
+    .duration(2000);
+
+  event.remove({ id: "architects_palette:ancient_plating" });
+  event.recipes.gtceu
+    .mixer("ancient_plating")
+    .itemInputs(
+      "minecraft:ancient_debris",
+      "4x architects_palette:nether_brass_ingot",
+      "4x architects_palette:nether_brass_nugget"
+    )
+    .itemOutputs("32x architects_palette:ancient_plating")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("ancient_plating")
+    .itemInputs(
+      "minecraft:ancient_debris",
+      "4x architects_palette:nether_brass_ingot"
+    )
+    .itemOutputs("64x architects_palette:ancient_plating")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:tread_plate" });
+  event.recipes.gtceu
+    .mixer("tread_plate")
+    .itemInputs("architects_palette:plating_block", "#forge:dusts/iron")
+    .itemOutputs("2x architects_palette:tread_plate")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("tread_plate")
+    .itemInputs("architects_palette:plating_block", "#forge:dusts/iron")
+    .itemOutputs("4x architects_palette:tread_plate")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:hazard_block" });
+  event.recipes.gtceu
+    .mixer("hazard_block")
+    .itemInputs("yellow_concrete", "black_concrete")
+    .itemOutputs("2x architects_palette:hazard_block")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("hazard_block")
+    .itemInputs("yellow_concrete", "black_concrete")
+    .itemOutputs("4x architects_palette:hazard_block")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:sheet_metal_block" });
+  event.recipes.gtceu
+    .compressor("sheet_metal_block")
+    .itemInputs("iron_block")
+    .itemOutputs("32x architects_palette:sheet_metal_block")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("sheet_metal_block")
+    .itemInputs("iron_block")
+    .itemOutputs("64x architects_palette:sheet_metal_block")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:oracle_lamp" });
+  event.recipes.gtceu
+    .mixer("oracle_lamp")
+    .itemInputs("architects_palette:oracle_jelly", "minecraft:end_rod")
+    .itemOutputs("architects_palette:oracle_lamp")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("oracle_lamp")
+    .itemInputs("architects_palette:oracle_jelly", "minecraft:end_rod")
+    .itemOutputs("2x architects_palette:oracle_lamp")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:dark_oracle_bricks" });
+  event.recipes.gtceu
+    .mixer("dark_oracle_bricks")
+    .itemInputs("architects_palette:oracle_bricks", "#forge:dyes/black")
+    .itemOutputs("architects_palette:dark_oracle_bricks")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("dark_oracle_bricks")
+    .itemInputs("architects_palette:oracle_bricks", "#forge:dyes/black")
+    .itemOutputs("2x architects_palette:dark_oracle_bricks")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:framed_oracle_block" });
+  event.recipes.gtceu
+    .mixer("framed_oracle_block")
+    .itemInputs(
+      "architects_palette:oracle_block",
+      "architects_palette:cerebral_plate"
+    )
+    .itemOutputs("2x architects_palette:framed_oracle_block")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("framed_oracle_block")
+    .itemInputs(
+      "architects_palette:oracle_block",
+      "architects_palette:cerebral_plate"
+    )
+    .itemOutputs("4x architects_palette:framed_oracle_block")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:algal_blend" });
+  event.recipes.gtceu
+    .mixer("algal_blend")
+    .itemInputs("clay_ball", "kelp")
+    .itemOutputs("2x architects_palette:algal_blend")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("algal_blend")
+    .itemInputs("clay_ball", "kelp")
+    .itemOutputs("4x architects_palette:algal_blend")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:entwine_rod" });
+  event.recipes.gtceu
+    .lathe("entwine_rod")
+    .itemInputs("minecraft:ender_pearl")
+    .itemOutputs("2x architects_palette:entwine_rod")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("entwine_rod")
+    .itemInputs("ender_pearl")
+    .itemOutputs("4x architects_palette:entwine_rod")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:sunmetal_blend" });
+  event.recipes.gtceu
+    .mixer("sunmetal_blend")
+    .itemInputs("soul_sand", "#forge:dusts/gold")
+    .itemOutputs("4x architects_palette:sunmetal_blend")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("sunmetal_blend")
+    .itemInputs("soul_sand", "#forge:dusts/gold")
+    .itemOutputs("8x architects_palette:sunmetal_blend")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:nether_brass_blend" });
+  event.recipes.gtceu
+    .mixer("nether_brass_blend")
+    .itemInputs("soul_sand", "#forge:dusts/brass")
+    .itemOutputs("4x architects_palette:nether_brass_blend")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("nether_brass_blend")
+    .itemInputs("soul_sand", "#forge:dusts/brass")
+    .itemOutputs("8x architects_palette:nether_brass_blend")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:wardstone" });
+  event.recipes.gtceu
+    .mixer("wardstone")
+    .itemInputs("nether_wart", "#forge:dusts/lapis")
+    .itemOutputs("4x architects_palette:wardstone")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("wardstone")
+    .itemInputs("nether_wart", "#forge:dusts/lapis")
+    .itemOutputs("8x architects_palette:wardstone")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:cerebral_plate" });
+  event.recipes.gtceu
+    .mixer("cerebral_plate")
+    .itemInputs("tuff", "#forge:dusts/nether_quartz")
+    .itemOutputs("4x architects_palette:cerebral_plate")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("cerebral_plate")
+    .itemInputs("tuff", "#forge:dusts/nether_quartz")
+    .itemOutputs("8x architects_palette:cerebral_plate")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+
+  event.remove({ id: "architects_palette:oracle_jelly" });
+  event.recipes.gtceu
+    .mixer("oracle_jelly")
+    .itemInputs("sugar", "#forge:dusts/amethyst")
+    .itemOutputs("4x architects_palette:oracle_jelly")
+    .EUt(2)
+    .duration(20);
+  event.recipes.gtceu
+    .construction_core("oracle_jelly")
+    .itemInputs("sugar", "#forge:dusts/amethyst")
+    .itemOutputs("8x architects_palette:oracle_jelly")
+    .inputFluids("gtceu:construction_foam 100")
+    .EUt(8)
+    .duration(20);
+});
---
+++ kubejs/server_scripts/xtones.js
@@ -0,0 +1,88 @@
+ServerEvents.recipes((event) => {
+  const dyes = [
+    "white",
+    "orange",
+    "magenta",
+    "light_blue",
+    "yellow",
+    "lime",
+    "pink",
+    "gray",
+    "light_gray",
+    "cyan",
+    "purple",
+    "blue",
+    "brown",
+    "green",
+    "red",
+    "black",
+  ];
+
+  function createRecipe(input1, input2, output, modifier) {
+    event.remove({ id: output });
+    event.recipes.gtceu
+      .mixer(output)
+      .itemInputs(input1, input2)
+      .itemOutputs(`${modifier}x ${output}`)
+      .EUt(2)
+      .duration(20);
+    event.recipes.gtceu
+      .construction_core(output)
+      .itemInputs(input1, input2)
+      .itemOutputs(`${modifier * 2}x ${output}`)
+      .inputFluids("gtceu:construction_foam 100")
+      .EUt(8)
+      .duration(20);
+  }
+  // @xtones -Variant
+
+  createRecipe("stone", "cobblestone", "xtonesreworked:xtone_tile", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/stone", "xtonesreworked:agon_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/blue", "xtonesreworked:azur_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/coal", "xtonesreworked:bitt_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/clay", "xtonesreworked:cray_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/brick", "xtonesreworked:fort_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/glass", "xtonesreworked:glaxx_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/deepslate", "xtonesreworked:iszm_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/orange", "xtonesreworked:jelt_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/flint", "xtonesreworked:korp_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "gtceu:fertilizer", "xtonesreworked:kryp_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/netherrack", "xtonesreworked:lair_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/ice", "xtonesreworked:lave_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/lime", "xtonesreworked:mint_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/wood", "xtonesreworked:myst_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/redstone", "xtonesreworked:reds_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/sugar", "xtonesreworked:reed_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/quartz_sand", "xtonesreworked:roen_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/yellow", "xtonesreworked:sols_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/green", "xtonesreworked:sync_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/gray", "xtonesreworked:tank_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/black", "xtonesreworked:vect_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/light_blue", "xtonesreworked:vena_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/clay", "xtonesreworked:zane_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/iron", "xtonesreworked:zech_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "snowball", "xtonesreworked:zest_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "string", "xtonesreworked:zeta_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/charcoal", "xtonesreworked:zion_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "bone", "xtonesreworked:zkul_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/andesite", "xtonesreworked:zoea_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/gunpowder", "xtonesreworked:zome_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dusts/granite", "xtonesreworked:zone_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "terracotta", "xtonesreworked:zorg_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "#forge:dyes/light_gray", "xtonesreworked:ztyl_block_0", 4);
+  createRecipe("xtonesreworked:xtone_tile", "stick", "xtonesreworked:zyth_block_0", 4);
+
+  event.remove({id: "xtonesreworked:zane_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zone_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zoea_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zyth_block_0_alternative"})
+  event.remove({id: "xtonesreworked:myst_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zest_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zkul_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zome_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zion_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zorg_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zeta_block_0_alternative"})
+  event.remove({id: "xtonesreworked:zech_block_0_alternative"})
+
+});
