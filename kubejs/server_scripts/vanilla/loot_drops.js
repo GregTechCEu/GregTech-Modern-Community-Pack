2c5dcf81ec9e2cdddf06af63daf833a61ff732e9
ursamina <143211829+ursamina@users.noreply.github.com>
2024-03-24 18:49:31 +0000

    remove vanilla gold farm and iron farm

--- kubejs/server_scripts/vanilla/loot_drops.js
+++ kubejs/server_scripts/vanilla/loot_drops.js
@@ -5,4 +5,13 @@ LootJS.modifiers((event) => {
   event
     .addEntityLootModifier("minecraft:enderman")
     .replaceLoot("minecraft:ender_pearl", "gtceu:ender_pearl_dust", true);
+
+    event
+    .addEntityLootModifier("minecraft:zombified_piglin")
+    .removeLoot("minecraft:gold_nugget");
+
+    
+  event
+  .addEntityLootModifier("minecraft:iron_golem")
+  .removeLoot("minecraft:iron_ingot");
 });
