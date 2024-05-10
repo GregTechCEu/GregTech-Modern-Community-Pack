3bd054a24200cb79eefc1326a777341c38b16a87
ursamina <143211829+ursamina@users.noreply.github.com>
2024-05-10 20:36:51 +0100

    deprecate angel ring

--- kubejs/server_scripts/elden_ring.js
+++ kubejs/server_scripts/elden_ring.js
@@ -1,13 +1,5 @@
 ServerEvents.recipes((event) => {
-  event.recipes.gtceu
-    .assembler("angel_ring")
-    .itemInputs("#forge:rings/rose_gold", "2x feather")
-    .inputFluids("gtceu:liquid_air 500")
-    .itemOutputs("better_angel_ring:angel_ring")
-    .duration(200)
-    .EUt(128);
-
-  // Void Portal
+// Void Portal
   event.remove({ id: "javd:portal_block" });
   event.shaped(`javd:portal_block`, ["PPP", "PEP", "PPP"], {
     P: "#forge:plates/obsidian",
