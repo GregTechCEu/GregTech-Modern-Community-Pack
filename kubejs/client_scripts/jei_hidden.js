523760ed519f821f9e42936aac350116a3d54eac
ursamina <143211829+ursamina@users.noreply.github.com>
2024-07-06 20:24:15 +0100

    kjs hidden category script fix
    remove redundant information, make filename make sense

--- kubejs/client_scripts/jei_hidden.js
+++
@@ -1,6 +0,0 @@
-JEIEvents.removeCategories(event => {
-  console.log(event.getCategoryIds()) //log a list of all category ids to logs/kubejs/client.txt
-  event.remove("lychee:item_inside/minecraft/default")
-  event.remove("jei:information")
-  event.remove("ae2:certus_growth")
-})
---
+++ kubejs/client_scripts/jei_hidden_categories.js
@@ -0,0 +1,5 @@
+JEIEvents.removeCategories(event => {
+  // console.log(event.getCategoryIds()) // log a list of all category ids to logs/kubejs/client.txt
+  event.remove("jei:information")
+  event.remove("ae2:certus_growth")
+})
