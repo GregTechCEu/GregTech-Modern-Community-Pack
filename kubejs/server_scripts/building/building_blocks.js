a8639b75ac22802a7f8857dd1c554b99479030ec
ursamina <143211829+ursamina@users.noreply.github.com>
2024-07-06 20:35:04 +0100

    add recipe for skystone
    skystone dust made in the classic construction core manner

---
+++ kubejs/server_scripts/building/building_blocks.js
@@ -0,0 +1,18 @@
+ServerEvents.recipes((event) => {
+  event.recipes.gtceu
+    .mixer("skystone_dust")
+    .itemInputs("gtceu:stone_dust", "#forge:dusts/certus_quartz")
+    .itemOutputs("2x ae2:sky_dust")
+    .circuit(1)
+    .EUt(2)
+    .duration(20);
+
+
+  event.recipes.gtceu
+    .construction_core("skystone_dust")
+    .itemInputs("gtceu:stone_dust", "#forge:dusts/certus_quartz")
+    .itemOutputs("8x ae2:sky_dust")
+    .circuit(1)
+    .EUt(2)
+    .duration(20);
+})
