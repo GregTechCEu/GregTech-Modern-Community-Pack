165eed9e603f0273d97c4bd3b7908ddaa5b6fb20
ursamina <forexeys@gmail.com>
2025-04-02 23:21:11 +0100

    Refactor client scripts
    Easier script structure and makes more sense :)

---
+++ kubejs/client_scripts/jei/hidden_items.js
@@ -0,0 +1,47 @@
+JEIEvents.hideItems(event => {
+
+  // Hide AE2 facades
+  event.hide('ae2:facade')
+
+  // Hide all irrelevant stonetype ores
+  event.hide('#forge:ores_in_ground/sand')
+  event.hide('#forge:ores_in_ground/red_sand')
+  event.hide('#forge:ores_in_ground/diorite')
+  event.hide('#forge:ores_in_ground/andesite')
+  event.hide('#forge:ores_in_ground/gravel')
+  event.hide('#forge:ores_in_ground/basalt')
+  event.hide('#forge:ores_in_ground/tuff')
+  event.hide('#forge:ores_in_ground/blackstone')
+  event.hide('#forge:ores_in_ground/marble')
+  event.hide('#forge:ores_in_ground/granite')
+  event.hide('#forge:ores_in_ground/red_granite')
+
+  // Miscellaneous item hiding
+  event.hide('ae2:ender_dust')
+  event.hide('ae2:certus_quartz_dust')
+  event.hide('ae2:certus_quartz_crystal')
+  event.hide('ae2:fluix_dust')
+  event.hide('ae2:silicon')
+
+  event.hide('ae2:flawless_budding_quartz')
+  event.hide('ae2:flawed_budding_quartz')
+  event.hide('ae2:chipped_budding_quartz')
+  event.hide('ae2:damaged_budding_quartz')
+
+  event.hide('ae2:small_quartz_bud')
+  event.hide('ae2:medium_quartz_bud')
+  event.hide('ae2:large_quartz_bud')
+  event.hide('ae2:quartz_cluster')
+
+  // Hide any evidence of the eAE multiblock
+  event.hide('expatternprovider:assembler_matrix_frame')
+  event.hide('expatternprovider:assembler_matrix_wall')
+  event.hide('expatternprovider:assembler_matrix_pattern')
+  event.hide('expatternprovider:assembler_matrix_speed')
+  event.hide('expatternprovider:assembler_matrix_crafter')
+  event.hide('expatternprovider:assembler_matrix_glass')
+
+  event.hide('expatternprovider:circuit_cutter')
+
+
+})
