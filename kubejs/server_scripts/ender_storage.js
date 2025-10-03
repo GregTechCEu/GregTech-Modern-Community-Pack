5764fb940eb7171f0a67846d605e5ae67575f87a
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2025-10-03 07:23:18 +0100

    Deprecate Ender Storage (#143)

--- kubejs/server_scripts/ender_storage.js
+++ kubejs/server_scripts/ender_storage.js
@@ -1,21 +1,13 @@
 ServerEvents.recipes((event) => {
   // Ender Tank
   event.remove({ id: "enderstorage:ender_tank" });
-  event.shaped(`enderstorage:ender_tank`, ["PEP", "STM", "PEP"], {
-    P: "#forge:plates/stainless_steel",
-    E: "#forge:plates/ender_pearl",
-    S: "gtceu:hv_sensor",
-    T: "gtceu:mv_super_tank",
-    M: "gtceu:hv_emitter"
-  })
+  event.shapeless(`gtceu:ender_fluid_link_cover`, [
+    "enderstorage:ender_tank"
+  ]);
   // Ender Chest
   event.remove({ id: "enderstorage:ender_chest" });
-  event.shaped(`enderstorage:ender_chest`, ["PEP", "MTS", "PEP"], {
-    P: "#forge:plates/stainless_steel",
-    E: "#forge:plates/ender_pearl",
-    S: "gtceu:hv_sensor",
-    T: "gtceu:mv_super_chest",
-    M: "gtceu:hv_emitter"
-  })
+  event.shapeless(`gtceu:ender_item_link_cover`, [
+    "enderstorage:ender_chest"
+  ]);
   event.remove({ id: "enderstorage:ender_pouch" });
 })
