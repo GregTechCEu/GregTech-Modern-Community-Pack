56797192b2455a708ea716a5abcce49f08b69a3d
Mițca Dumitru <dumitru0mitca@gmail.com>
2025-12-14 23:11:03 +0200

    Allow using framed doors as the doors for a cleanroom

--- kubejs/server_scripts/tags/blocks.js
+++ kubejs/server_scripts/tags/blocks.js
@@ -1,3 +1,5 @@
 ServerEvents.tags("block", (event) => {
   event.add("forge:mineable/wrench", "travelanchors:travel_anchor");
+  event.add("gtceu:cleanroom_doors", "framedblocks:framed_door");
+  event.add("gtceu:cleanroom_doors", "framedblocks:framed_iron_door");
 });
