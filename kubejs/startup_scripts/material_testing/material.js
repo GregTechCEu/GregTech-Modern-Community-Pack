8b4767e31634a0df9f2ea6300c7544c0d7649f95
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2025-12-26 12:08:15 +0000

    missed some stuff

--- kubejs/startup_scripts/material_testing/material.js
+++ kubejs/startup_scripts/material_testing/material.js
@@ -5,7 +5,7 @@ GTCEuStartupEvents.registry("gtceu:material", (event) => {
     .components("1x nether_quartz", "1x certus_quartz", "1x redstone")
     .color(0x8f5ccb)
     .iconSet(GTMaterialIconSet.DULL)
-    .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_GEAR, GTMaterialFlags.FLAMMABLE);
+    .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_GEAR);
 
     /*
   event
