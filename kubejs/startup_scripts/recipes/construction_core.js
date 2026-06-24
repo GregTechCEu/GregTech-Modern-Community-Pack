e46cfe27a336b5f3b758b8ae93a6b7e756992872
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2026-06-24 18:23:15 +0100

    "fix" kjs so game actually loads

--- kubejs/startup_scripts/recipes/construction_core.js
+++ kubejs/startup_scripts/recipes/construction_core.js
@@ -4,7 +4,12 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", (event) => {
     .category("test")
     .setEUIO("in")
     .setMaxIOSize(2, 1, 1, 0)
-    .setSlotOverlay(false, false, GuiTextures.BOX_OVERLAY)
-    .setProgressBar(GuiTextures.PROGRESS_BAR_BATH, FillDirection.LEFT_TO_RIGHT)
+    // .setSlotOverlay(false, false, GuiTextures.BOX_OVERLAY) TODO Migrate to mui
+    // .setProgressBar(GuiTextures.PROGRESS_BAR_BATH, FillDirection.LEFT_TO_RIGHT)
+    // .UI((b) => {
+    //   b.setProgressBar(GTGuiTextures.PROGRESS_BATH)
+    //   .setItemSlotsOverlay(IO.IN, 0, 1, GTGuiTextures.BOX_OVERLAY)
+    //   .setItemSlotOverlay(IO.OUT, 0,  GTGuiTextures.BOX_OVERLAY);
+    // })
     .setSound(GTSoundEntries.COOLING);
 });
--- kubejs/startup_scripts/recipes/greenhouse.js
+++ kubejs/startup_scripts/recipes/greenhouse.js
@@ -4,7 +4,12 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", (event) => {
     .category("test")
     .setEUIO("in")
     .setMaxIOSize(3, 4, 1, 0)
-    .setSlotOverlay(false, false, GuiTextures.BOX_OVERLAY)
-    .setProgressBar(GuiTextures.PROGRESS_BAR_BATH, FillDirection.LEFT_TO_RIGHT)
+    // .setSlotOverlay(false, false, GuiTextures.BOX_OVERLAY) TODO Migrate to mui
+    // .setProgressBar(GuiTextures.PROGRESS_BAR_BATH, FillDirection.LEFT_TO_RIGHT)
+    // .UI((b) => {
+    //   b.setProgressBar(GTGuiTextures.PROGRESS_BATH)
+    //   .setItemSlotsOverlay(IO.IN, 0, 2, GTGuiTextures.BOX_OVERLAY)
+    //   .setItemSlotsOverlay(IO.OUT, 0, 3, GTGuiTextures.BOX_OVERLAY);
+    // })
     .setSound(GTSoundEntries.COOLING);
 });
