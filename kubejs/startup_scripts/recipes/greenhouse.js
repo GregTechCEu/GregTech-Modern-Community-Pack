97e72cab91d2b6dfd4e51387b01eddc8f122e163
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2026-06-24 19:49:07 +0100

    fix the multi recipe uis
    thanks onion

--- kubejs/startup_scripts/recipes/greenhouse.js
+++ kubejs/startup_scripts/recipes/greenhouse.js
@@ -4,12 +4,10 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", (event) => {
     .category("test")
     .setEUIO("in")
     .setMaxIOSize(3, 4, 1, 0)
-    // .setSlotOverlay(false, false, GuiTextures.BOX_OVERLAY) TODO Migrate to mui
-    // .setProgressBar(GuiTextures.PROGRESS_BAR_BATH, FillDirection.LEFT_TO_RIGHT)
-    // .UI((b) => {
-    //   b.setProgressBar(GTGuiTextures.PROGRESS_BATH)
-    //   .setItemSlotsOverlay(IO.IN, 0, 2, GTGuiTextures.BOX_OVERLAY)
-    //   .setItemSlotsOverlay(IO.OUT, 0, 3, GTGuiTextures.BOX_OVERLAY);
-    // })
+    .ui((b) => {
+      b.setProgressBar(GTGuiTextures.PROGRESS_ASSEMBLER) // TODO Change back to chem bath overlay
+      .setItemSlotsOverlay(IO.IN, 0, 2, GTGuiTextures.BOX_OVERLAY)
+      .setItemSlotsOverlay(IO.OUT, 0, 3, GTGuiTextures.BOX_OVERLAY);
+    })
     .setSound(GTSoundEntries.COOLING);
 });
