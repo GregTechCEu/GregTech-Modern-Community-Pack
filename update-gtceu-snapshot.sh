4110157e218cd1845ff2ea88c04c30425354aa7d
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2026-06-24 10:04:16 +0100

    add script to update gt to latest 8.0 snapshot

---
+++ update-gtceu-snapshot.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version=$(curl https://maven.gtceu.com/com/gregtechceu/gtceu/gtceu-1.20.1/8.0.0-SNAPSHOT/maven-metadata.xml | xmllint --xpath '/metadata/versioning/snapshotVersions/snapshotVersion[extension[text()="jar"]][not(./classifier)]/value/text()' -)
+url=https://maven.gtceu.com/com/gregtechceu/gtceu/gtceu-1.20.1/8.0.0-SNAPSHOT/gtceu-1.20.1-$version.jar
+packwiz url add gregtechceu-modern $url
