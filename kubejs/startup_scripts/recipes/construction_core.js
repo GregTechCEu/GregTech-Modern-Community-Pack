7bcf090fe768b1b8b2d7d03a5f9e3737026856f1
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2026-08-27 17:00:05 +0100

    Update GT to latest

--- kubejs/startup_scripts/recipes/construction_core.js
+++ kubejs/startup_scripts/recipes/construction_core.js
@@ -4,10 +4,8 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", (event) => {
     .category("test")
     .setEUIO("in")
     .setMaxIOSize(2, 1, 1, 0)
-    .ui((b) => {
-      b.setProgressBar(GTGuiTextures.PROGRESS_ASSEMBLER)
-      .setItemSlotsOverlay(IO.IN, 0, 1, GTGuiTextures.BOX_OVERLAY)
-      .setItemSlotOverlay(IO.OUT, 0,  GTGuiTextures.BOX_OVERLAY);
-    })
+    .setProgressBar(GTGuiTextures.PROGRESS_ASSEMBLER)
+    .setItemSlotsOverlay(IO.IN, 0, 1, GTGuiTextures.BOX_OVERLAY)
+    .setItemSlotOverlay(IO.OUT, 0,  GTGuiTextures.BOX_OVERLAY)
     .setSound(GTSoundEntries.COOLING);
 });
--- kubejs/startup_scripts/recipes/greenhouse.js
+++ kubejs/startup_scripts/recipes/greenhouse.js
@@ -4,10 +4,8 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", (event) => {
     .category("test")
     .setEUIO("in")
     .setMaxIOSize(3, 4, 1, 0)
-    .ui((b) => {
-      b.setProgressBar(GTGuiTextures.PROGRESS_ASSEMBLER) // TODO Change back to chem bath overlay
-      .setItemSlotsOverlay(IO.IN, 0, 2, GTGuiTextures.BOX_OVERLAY)
-      .setItemSlotsOverlay(IO.OUT, 0, 3, GTGuiTextures.BOX_OVERLAY);
-    })
+    .setProgressBar(GTGuiTextures.PROGRESS_ASSEMBLER) // TODO Change back to chem bath overlay
+    .setItemSlotsOverlay(IO.IN, 0, 2, GTGuiTextures.BOX_OVERLAY)
+    .setItemSlotsOverlay(IO.OUT, 0, 3, GTGuiTextures.BOX_OVERLAY)
     .setSound(GTSoundEntries.COOLING);
 });
